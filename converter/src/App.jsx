0fb12316de8de03714144ae9774a87ea5d864675
dadhy <evgenzolotoff@gmail.com>
2022-05-26 00:57:15 +0400

    add

---
+++ converter/src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Converter from './components/converter'
+import './styles/index.css'
+
+function App() {
+
+  return (
+    <div>
+      <div className="max-w-4xl m-auto pb-6 p">
+        <Converter />
+      </div>
+    </div>
+  )
+}
+
+export default App
